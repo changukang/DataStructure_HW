cc7efa22799882c8d5c67ae122568f3b4f5fa87a
changukang <nachacham@gmail.com>
2017-06-08 02:23:26 +0900

    .

--- HW04.java
+++ HW04.java
@@ -7,55 +7,43 @@ import java.lang.Double;
 
 class Node {
   public int val;
-  public Node left;
-  public Node right;
-  public Node p;
-  public int color;
+  public Node left, right, p;
+  public int color=0;
   /////
   /// color RED is assigned to integer 1
   /// color BLACK is assigned to integer 0
   /////
-  public Node(){
-  	val = 0;
+  public Node(int newval) {
+    val = newval;
     left = null;
     right = null;
     p = null;
-    color = 0;
-  }
-
-  public static Node Nil(){
-  	return new Node();
-  }	
-
-  public Node(int newval) {
-    val = newval;
-    left = Nil();
-    right = Nil();
-    left.p = this;
-    right.p = this;
-    p = Nil();
-    color = 1;
   }
 }
 
 class RB{
+<<<<<<< HEAD
 	public int[] none_found = new int[1000]; //없느 노드를 기록하기 위함
 	public int index = 0;
 	public Node root;
+=======
+	public Node root = null;
+	//나중에 RB.nill 추가 예정
+>>>>>>> parent of 5352e3d... changed up the code
 	public RB(){
-		root = Node.Nil();
+		root = null;
 	}
 
 	public void Left_Rotate(Node x){
 		Node y = x.right;
 		x.right = y.left;
 
-		if(y.left.val != 0){
+		if(y.left!=null){
 			y.left.p = x;
 		}
 		y.p = x.p;
-		if(x.p.val == 0){
-			root = y;
+		if(x.p == null){
+			this.root = y;
 		}
 		else if(x==x.p.left){
 			x.p.left =y;
@@ -70,17 +58,18 @@ class RB{
 	public void Right_Rotate(Node x){
 		Node y = x.left;
 		x.left = y.right;
-		if(y.right.val != 0){
+		if(y.right!=null){
 			y.right.p = x;
 		}
 		y.p = x.p;
-		if(x.p.val == 0){
-			root = y;
+		if(x.p == null){
+			this.root = y;
 		}
 		else if( x==x.p.right ){
 			x.p.right =y;
 		}
 		else{
+
 			x.p.left = y;
 		}
 		y.right = x;
@@ -88,15 +77,28 @@ class RB{
 	}
 
 	public void RB_INSERT_FIXUP(Node z){
-		Node y;
-			while( z.p.color == 1){
+		Node y = null;
+		if(z.p == null){ 	//
+			this.root = z;	//
+			z.color = 0;	//
+			return;			//
+		}
+		else if(z.p != null){
+			while( z.p != null && z.p.color == 1){
+				//following code is for left
+				if(z.p.p == null){
+					break; //this is the case when z.p is root
+				}
 				if(z.p == z.p.p.left){
 					y = z.p.p.right; //y 는 삼촌
-					if(y.color == 1){ //CASE 1
+					if(y!= null && y.color == 1){ //CASE 1
 						z.p.color = 0;
 						y.color = 0;
 						z.p.p.color = 1;
 						z = z.p.p;
+						if(z == this.root){
+							z.color = 0;//by the rule of RB, root node must be BLACK which is integer 0
+						}
 					}
 					else
 					{
@@ -112,35 +114,39 @@ class RB{
 				}
 				////Following code is for right
 				else{
-				if(z.p == z.p.p.right){
 					y = z.p.p.left; //y 는 삼촌
-					if(y.color == 1){ //CASE 1
+					if(y != null && y.color == 1){ //CASE 1
 						z.p.color = 0;
 						y.color = 0;
 						z.p.p.color = 1;
 						z = z.p.p;
+						if(z == this.root){
+							z.color = 0; //by the rule of RB, root node must be BLACK which is integer 0
+						}
 					}
-					else
-					{
-					 if (z==z.p.left){ //CASE 2
+					else {
+					if (z==z.p.left){ //CASE 2
 						z = z.p;
 						Right_Rotate(z);
+
 					}
-					z.p.color = 0;//CASE 3
+					z.p.color = 0; //CASE 3
 					z.p.p.color = 1;
 					Left_Rotate(z.p.p);
 				}
 			}
+
+			this.root.color = 0; //To ensure the root node of Tree is black
+			}
 		}
-		}
-		root.color = 0; 
 	}
 
 
 	public void RB_insert(Node z){
-		Node y = Node.Nil();
-		Node x = root;
-    while (x.val != 0){
+	Node y = null;
+	Node x = this.root;
+
+    while (x!= null){
       y = x;
       if (z.val < x.val){
         x = x.left;
@@ -151,8 +157,8 @@ class RB{
     }
 
     z.p = y;
-    if (y.val == 0){
-      root = z;
+    if (y == null){
+      this.root = z;
     }
     else if (z.val < y.val){
       y.left = z;
@@ -160,30 +166,30 @@ class RB{
     else{
       y.right=z;
     }
-    // z.left = Node.Nil();
-    // z.right = Node.Nil();
-    // z.color = 1;
+	z.left = null;
+	z.right = null;
+	z.color = 1;
 
 	RB_INSERT_FIXUP(z);
 
 	}
 
-    // public void tree_print(Node tree, int level) {
+    public void tree_print(Node tree, int level) {
 
-	   //  if (tree.right != null)
-	   //    tree_print(tree.right, level + 1);
+	    if (tree.right != null)
+	      tree_print(tree.right, level + 1);
 
-	   //  for(int i = 0; i < level; i++)
-	   //    System.out.print("        ");
+	    for(int i = 0; i < level; i++)
+	      System.out.print("        ");
 
-	   //  System.out.println(tree.val+""+tree.color);
-	   //  if (tree.left != null)
-	   //    tree_print(tree.left, level + 1);
-    // }
+	    System.out.println(tree.val+""+tree.color);
+	    if (tree.left != null)
+	      tree_print(tree.left, level + 1);
+    }
 
    public void RB_DELETE_FIXUP(Node x){
-   	Node w;
-  	while(x!=root && x.color == 0){
+  	Node w =  null;
+  	while(x!=this.root && x.color == 0){
   		if(x==x.p.left){
   			w = x.p.right;
   			if(w.color == 1){
@@ -192,7 +198,7 @@ class RB{
   				Left_Rotate(x.p);
   				w=x.p.right;
   			}
-  			if(w.left.color == 0 && w.right.color == 0){
+  			if(w.left.color == 0 && w.right.color ==0){
   				w.color =1;
   				x=x.p;
   			}
@@ -202,12 +208,13 @@ class RB{
   					w.color = 1;
   					Right_Rotate(w);
   					w = x.p.right;
+
   				}
   				w.color = x.p.color;
   				x.p.color = 0;
   				w.right.color = 0;
   				Left_Rotate(x.p);
-  				x = root;
+  				x = this.root;
   			}
   		}
   		else{
@@ -219,7 +226,7 @@ class RB{
   				w=x.p.left;
   			}
   			if(w.right.color == 0 && w.left.color ==0){
-  				w.color = 1;
+  				w.color =1;
   				x=x.p;
   			}
   			else{
@@ -234,24 +241,26 @@ class RB{
   				x.p.color = 0;
   				w.left.color = 0;
   				Right_Rotate(x.p);
-  				x = root;
+  				x = this.root;
   			}
   		}
-	}
-	x.color = 0;
 
+  		if(x!=null)
+  			x.color = 0;
+		}
   	}
 
 	public Node tree_min(Node n){
-	    while(n.left.val != 0){
+	    while(n.left != null){
 	      n = n.left;
 	    }
 	    return n;
 	  }
 
 	public void RB_Transplant(Node u, Node v){
-	  	if(u.p.val==0){
-	  		root = v;
+	  	if(u.p==null){
+	  		v.color =0;
+	  		this.root = v;
 	  	}
 	  	else if(u == u.p.left){
 	  		u.p.left = v;
@@ -259,21 +268,24 @@ class RB{
 	  	else{
 	  		u.p.right = v;
 	  	}
+	  	if(u.right ==null && u.left == null){
+	  		return;
+	  	}
 	  	v.p = u.p;
 	 }
 
 
 	public void RB_delete(Node z){
-		Node y = z;
+	 	Node y = z;
  	 	int y_org_col = y.color;
- 	 	Node x;
+ 	 	Node x = null;
 	 	//checker
 	 	// System.out.println(z.left);
-	 	if(z.left.val == 0){
+	 	if(z.left==null){
 	 		x = z.right;
 	 		RB_Transplant(z,z.right);
 	 	}
-	 	else if(z.right.val == 0){
+	 	else if(z.right == null){
 	 		x = z.left;
 	 		RB_Transplant(z, z.left);
 	 	}
@@ -282,7 +294,10 @@ class RB{
 	 		y_org_col = y.color;
 	 		x = y.right;
 	 		if(y.p == z){
-	 		 x.p=y;
+	 			if(x==null){
+	 				//do nothing
+	 			}
+	 			else x.p=y;
 	 		}
 	 		else{
 	 			RB_Transplant(y,y.right);
@@ -291,18 +306,21 @@ class RB{
 	 		}
 	 		RB_Transplant(z,y);
 	 		y.left = z.left;
-	 		y.left.p = y;
+	 		z.left.p = y;
 	 		y.color = z.color;
 	 	}
+
 	 	if(y_org_col == 0){
+	 		if(x!=null){
 	 		RB_DELETE_FIXUP(x);
+	 	}
 	 	}
 
 	 }
 
 	 public Node tree_search(Node tree, int val) {
-	 	if (tree.val == 0)
-	      return Node.Nil();
+	    if (tree == null)
+	      return null;
 	    else if (val == tree.val)
 	      return tree;
 	    else if (val < tree.val)
@@ -313,7 +331,7 @@ class RB{
 
 
   	public void inorder(Node tree) {
-    	if (tree.val == 0)
+    	if (tree == null)
       		return;
    		else {
 	      inorder(tree.left);
@@ -324,23 +342,22 @@ class RB{
 
  	 public int NodeCount(Node tree){
  	 	int c = 1;
- 	 	if( tree.right.val!=0) { c+= NodeCount(tree.right); }
- 	 	if( tree.left.val!=0) { c+= NodeCount(tree.left); }
-
+ 	 	if( tree.right!=null) { c+= NodeCount(tree.right); }
+ 	 	if( tree.left!=null) { c+= NodeCount(tree.left); }
  	 	return c;
   	}
 
 	public int BlackNodeCount(Node tree){	
 		int c = 0;
-		if(tree.left.val != 0) c+=BlackNodeCount(tree.left);
-		if(tree.right.val!=0) c+=BlackNodeCount(tree.right);
+		if(tree.left!=null) c+=BlackNodeCount(tree.left);
+		if(tree.right!=null) c+=BlackNodeCount(tree.right);
 		if(tree.color == 0) c+=1;
 		return c;
 	}
 
 
 	public int BlackHeight(Node tree){
-		if(tree.val == 0){
+		if(tree==null){
 			return 0;
 		}
 		else if(tree.color==0){
@@ -355,13 +372,12 @@ class RB{
 
 public class HW04 {
     public static void main(String[] args) throws IOException {
-    	int index = 1;
     	 RB temp = new RB();
+
     	 //Getting input from txt file
         BufferedReader br = new BufferedReader(new FileReader("input.txt"));
         while(true) {
             String line = br.readLine();
-            // System.out.println(line);
             if (line==null) break;
             // int num = Integer.parseInt(line);
             double d = Double.parseDouble(line);
@@ -370,6 +386,7 @@ public class HW04 {
             	temp.RB_insert(new Node(num));
             }
             else if(num<0){
+<<<<<<< HEAD
             	Node del;
             	del = temp.tree_search(temp.root, num *= -1);
 <<<<<<< HEAD
@@ -387,18 +404,29 @@ public class HW04 {
            			temp.index += 1;
            		}
            	}
+=======
+            	Node del = null;
+            	del = temp.tree_search(temp.root,num *= -1);
+            	if(del != null){
+           		temp.RB_delete(del);
+           		 }
+           		}
+>>>>>>> parent of 5352e3d... changed up the code
             else if(num==0){
             	break;
             }
 
-
         }
         br.close();
 
+
+        //instruction of ds13b.pdf : to show 1) num of node, 2) num of black node )black height 4) inorder traversal
         System.out.println("total = " + temp.NodeCount(temp.root));
         System.out.println("nb = " + temp.BlackNodeCount(temp.root));
         System.out.println("bh = " + temp.BlackHeight(temp.root));
+
         temp.inorder(temp.root);
+
     //Generated following code to check if alg works well in randomly status
 	// Random random = new Random();
 	// RB wh = new RB();
